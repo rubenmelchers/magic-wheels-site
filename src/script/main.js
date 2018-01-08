1558d5ff44d42fda58d3ab9e088166fd46a1a9cc
wmcheung <wesleylgr33@gmail.com>
2018-01-08 20:31:14 +0100

    building the site according to BEM namings, created menu, added images

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,15 @@
 (function() {
 	"use strict";
-
-	// console.log("test3");
-
-
+	
+	window.onscroll = function changeNav(){
+		var navigation = document.querySelector(".js-navigation");
+		var scrollPosY = window.pageYOffset | document.body.scrollTop;
+		
+		if(scrollPosY > 100) {
+			navigation.classList.add('navigation__scrolling');
+		} else if(scrollPosY <= 100) {
+			navigation.classList.remove('navigation__scrolling');
+		}
+		
+	}
 })();
